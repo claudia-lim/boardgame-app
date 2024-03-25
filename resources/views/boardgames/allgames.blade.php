90a65b19b2093da13fb72c54250c6116f04be8df
Claudia Lim <climweiching@gmail.com>
2024-03-25 14:49:49 +0000

    view all boardgames

---
+++ resources/views/boardgames/allgames.blade.php
@@ -0,0 +1,9 @@
+<h1>All games currently in boardgames table:</h1>
+    @foreach($allGames as $game)
+        <div>
+
+        {{ $game['id'] }} : {{ $game['name'] }}
+        </div>
+
+    @endforeach
+
